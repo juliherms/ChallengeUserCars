ac5386f66f060da81a26ed0a18bfb6e44c79ecb4
Vasconcelos <j.a.vasconcelos@accenture.com>
2020-04-05 06:09:35 -0300

    RN01 - implementaçao da integracao continua

--- src/main/java/br/com/b3/controller/SessionController.java
+++ src/main/java/br/com/b3/controller/SessionController.java
@@ -1,5 +1,9 @@
 package br.com.b3.controller;
 
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -72,6 +76,7 @@ public class SessionController {
 		final String token = jwtTokenUtil.generateToken(userDetails);
 		final User user = userService.findByLogin(credenciaisDTO.getLogin());
 		user.setPassword(null);
+
 		
 		userService.saveLastLogin(user.getId());
 
--- src/main/java/br/com/b3/controller/UserController.java
+++ src/main/java/br/com/b3/controller/UserController.java
@@ -147,7 +147,5 @@ public class UserController {
 				}
 			}
 		}
-
 	}
-
 }
