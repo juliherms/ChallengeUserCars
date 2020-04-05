423355fb248df76c7ac0b78573c466de584c1222
Vasconcelos <j.a.vasconcelos@accenture.com>
2020-04-05 09:08:38 -0300

    RN01 - implementaçao da integracao continua

--- src/test/java/CarrosApplicationTest.java
+++ src/test/java/CarrosApplicationTest.java
@@ -1,18 +1,7 @@
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@ActiveProfiles("test")
 public class CarrosApplicationTest {
-	
-	@Test
-	public void contextLoads() {
-		
-	}
 
+	public void contextLoads() {
+
+	}
 
 }
