eb44e288fa691af3c9f54f293315e7c51975f479
Vasconcelos <j.a.vasconcelos@accenture.com>
2020-04-05 23:18:13 -0300

    RN04 - adicao do swagger

--- src/main/java/br/com/b3/config/SecurityConfig.java
+++ src/main/java/br/com/b3/config/SecurityConfig.java
@@ -65,7 +65,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	public void configure(WebSecurity web) throws Exception {
 		web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources/**", "/configuration/**",
-				"/swagger-ui.html", "/webjars/**");
+				"/swagger-ui.html", "/webjars/**"); 
 	}
 
 	/**
